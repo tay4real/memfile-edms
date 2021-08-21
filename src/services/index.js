f381826b44932c662ade97f7c723bf7e081f9f13
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2021-08-21 03:34:49 +0100

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-// export const fetchBackend = axios.create({
-//   baseURL:
-//     process.env.NOD_ENV === "production"
-//       ? process.env.REACT_APP_BACKEND_URL
-//       : process.env.REACT_APP_DEV_BACKEND_URL,
-//   headers: {
-//     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//     "Content-type": "application/json",
-//   },
-// });
-
 export const fetchBackend = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL:
+    process.env.NOD_ENV === "production"
+      ? process.env.REACT_APP_BACKEND_URL
+      : process.env.REACT_APP_DEV_BACKEND_URL,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     "Content-type": "application/json",
   },
 });
+
+// export const fetchBackend = axios.create({
+//   baseURL: process.env.REACT_APP_BACKEND_URL,
+//   headers: {
+//     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+//     "Content-type": "application/json",
+//   },
+// });
